c1e7ad9988bfa63479fd58476e14137a4c662196
Pete O'Shea <mail@peteoshea.co.uk>
2020-08-17 13:45:11 +0100

    Create Table component
    This just contains a placeholder at this stage but you can see it is actually being used on the spreadsheet route

--- src/routes/spreadsheet/[id].svelte
+++ src/routes/spreadsheet/[id].svelte
@@ -1,1 +1,13 @@
-test
+<script context="module">
+  export async function preload({ params }) {
+    const { id } = params;
+    return { id };
+  }
+</script>
+
+<script>
+  export let id;
+  import Table from '../../components/Table.svelte';
+</script>
+
+<Table tableIdentifier={id} />
