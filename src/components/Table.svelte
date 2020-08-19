d00b4988d059a96fdad9cec0468108e6a0fb515b
Pete O'Shea <mail@peteoshea.co.uk>
2020-08-19 08:13:51 +0100

    Persist the data to local storage

--- src/components/Table.svelte
+++ src/components/Table.svelte
@@ -1,12 +1,31 @@
 <script>
   import Row from './Row.svelte';
+  import { onMount } from 'svelte';
+
+  export let tableIdentifier;
 
   let columns = 8;
   let data = {};
   let rows = 15;
 
+  onMount(() => {
+    if (typeof window !== 'undefined') {
+      const localStorageData = window.localStorage.getItem(tableIdentifier);
+      if (localStorageData) {
+        data = JSON.parse(localStorageData);
+      }
+    }
+  });
+
   const handleChangedCell = (column, row, value) => {
-    console.log(column, row, value);
+    if (!data[row]) {
+      data[row] = {};
+    }
+    data[row][column] = value;
+
+    if (window && window.localStorage) {
+      window.localStorage.setItem(tableIdentifier, JSON.stringify(data));
+    }
   };
 </script>
 
